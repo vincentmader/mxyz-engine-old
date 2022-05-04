1b9389579944cf24a5d493cf06b623616e1bdf60
Vincent C. Mader <vincent.mader@icloud.com>
2022-05-04 02:16:53 +0200

    initialized System Config

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,10 +1,15 @@
+pub mod system;
+pub use system::SystemConfig;
+
 #[derive(Debug)]
 pub struct EngineConfig {
     pub step_id: usize,
+    pub systems: Vec<SystemConfig>,
 }
 impl EngineConfig {
     pub fn new() -> Self {
         let step_id = 0;
-        EngineConfig { step_id }
+        let systems = vec![];
+        EngineConfig { step_id, systems }
     }
 }
---
+++ src/config/system/mod.rs
@@ -0,0 +1,7 @@
+#[derive(Debug)]
+pub struct SystemConfig {}
+impl SystemConfig {
+    pub fn new() -> Self {
+        SystemConfig {}
+    }
+}
