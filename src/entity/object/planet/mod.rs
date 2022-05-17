8008593641328e24acc174611fadcf0136bba4c6
Vincent C. Mader <vincent.mader@icloud.com>
2022-05-17 19:16:44 +0200

    minor

--- src/entity/object/planet/mod.rs
+++ src/entity/object/planet/mod.rs
@@ -18,19 +18,25 @@ impl Planet {
     }
 }
 impl Mass for Planet {
-    fn set_mass(&mut self, mass: f64) {
-        self.mass = mass;
-    }
     fn get_mass(&self) -> f64 {
         self.mass
     }
+    fn set_mass(&mut self, mass: f64) {
+        self.mass = mass;
+    }
 }
 impl Position for Planet {
+    fn get_position(&self) -> &[f64; 3] {
+        &self.position
+    }
     fn set_position(&mut self, position: &[f64; 3]) {
         self.position = *position;
     }
 }
 impl Velocity for Planet {
+    fn get_velocity(&self) -> &[f64; 3] {
+        &self.velocity
+    }
     fn set_velocity(&mut self, velocity: &[f64; 3]) {
         self.velocity = *velocity;
     }
