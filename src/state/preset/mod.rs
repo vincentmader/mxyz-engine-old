170b6acfabb9f56742d5f46d736bb42615a58c40
Vincent C. Mader <vincent.mader@icloud.com>
2022-06-16 01:25:55 +0200

    first tests with pre-defined states (presets)

--- src/state/preset/mod.rs
+++ src/state/preset/mod.rs
@@ -1,11 +1,7 @@
+pub mod three_body_figure_eight;
 use crate::config::EngineConfig;
-use crate::entity;
-use crate::integrator::{Integrator, IntegratorVariant};
-use crate::interaction::force::{Force, ForceVariant};
-use crate::interaction::{Interaction, InteractionVariant};
-use crate::system::{System, SystemVariant};
-
-const NR_OF_STEPS: usize = 2221;
+use crate::system::System;
+use three_body_figure_eight::three_body_figure_eight;
 
 pub enum SimulationId {
     ThreeBodyFigureEight,
@@ -21,69 +17,4 @@ pub fn initialize(sim_id: &Option<SimulationId>, config: &mut EngineConfig) -> V
         },
     }
     systems
-}
-
-pub fn three_body_figure_eight(systems: &mut Vec<System>, config: &mut EngineConfig) {
-    // I. SYSTEMS
-    // ========================================================================
-    config.step_id.1 = NR_OF_STEPS;
-
-    // System 0: Objects
-    // ------------------------------------------------------------------------
-    let variant = SystemVariant::PhysicalObjects;
-    let mut system = System::new(variant);
-    let speed = 0.;
-    for entity_id in 0..2 {
-        let m = 1.;
-        let x = [2. * (entity_id as f64 - 0.5), 0., 0.];
-        let v = [0., speed * (2. * entity_id as f64 - 1.), 0.];
-        let entity = entity::object::planet::Planet::new(m, x, v);
-        system.entities.push(Box::new(entity));
-    }
-    systems.push(system);
-
-    // System 1: Field
-    // ------------------------------------------------------------------------
-    let variant = SystemVariant::PhysicalObjects;
-    let mut system = System::new(variant);
-    for _ in 0..2 {
-        let vel = [0., 0., 0.];
-        let dens = 1.;
-        let entity = entity::field::fluid_cell::FluidCell::new(vel, dens);
-        system.entities.push(Box::new(entity));
-    }
-    systems.push(system);
-
-    // III.INTEGRATORS
-    // ========================================================================
-
-    // System 0: Objects
-    // ------------------------------------------------------------------------
-    let mut integrators = vec![];
-    let mut integrator = Integrator::new(IntegratorVariant::EulerExplicit);
-    let mut interactions = vec![];
-    let force = Force::new(ForceVariant::NewtonianGravity);
-    let mut interaction = Interaction::new(InteractionVariant::Force(force));
-    interaction.matrix.init(&systems);
-    interaction.matrix.entries[0] = Some(true);
-    interactions.push(interaction);
-    integrator.interactions = interactions;
-    integrators.push(integrator);
-    config.integrators.push(integrators);
-
-    // System 1: Field
-    // ------------------------------------------------------------------------
-    let mut integrators = vec![];
-    let mut integrator = Integrator::new(IntegratorVariant::CellularAutomaton);
-    let mut interactions = vec![];
-    let force = Force::new(ForceVariant::NewtonianGravity);
-    let mut interaction = Interaction::new(InteractionVariant::Force(force));
-    interaction.matrix.init(&systems);
-    interaction.matrix.entries[0] = Some(true);
-    interactions.push(interaction);
-    integrator.interactions = interactions;
-    integrators.push(integrator);
-    config.integrators.push(integrators);
-
-    println!("\n\n{}", systems[0].entities.len());
 }
