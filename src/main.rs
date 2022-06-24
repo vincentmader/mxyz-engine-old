ae330021f672090fddce98c034b86a6458389920
Vincent C. Mader <vincent.mader@icloud.com>
2022-06-24 12:40:35 +0200

    minor

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use mxyz_engine::state::preset::SimulationId;
 use mxyz_engine::Engine;
 mod dev_utils;
 
-const EXPORT_VARIANT: ExportVariant = ExportVariant::ToFile;
+const EXPORT_VARIANT: ExportVariant = ExportVariant::ToFile; //
 
 fn main() {
     /// Creates & Initializes Engine
@@ -13,11 +13,11 @@ fn main() {
     engine.config.export_variant = EXPORT_VARIANT;
 
     /// Runs Engine & Records Execution Time
-    // dev_utils::print_state(&engine);
     let start_time = std::time::Instant::now();
     engine.run();
     let duration = start_time.elapsed().as_millis();
     println!("\nruntime:\t{} ms", duration);
+
     // dev_utils::print_state(&engine);
-    // dev_utils::print_interaction_matrix(&engine);
+    // dev_utils::_print_interaction_matrix(&engine);
 }
